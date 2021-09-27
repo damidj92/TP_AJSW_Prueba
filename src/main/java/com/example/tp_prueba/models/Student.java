8f70f553c2d4056682a23c23bcec3a85792bfd3a
damidj92 <damidj92@gmail.com>
2021-09-26 23:39:18 -0300

    Se agregaron las entidades de Course y StudentCourse

--- src/main/java/com/example/tp_prueba/models/Student.java
+++ src/main/java/com/example/tp_prueba/models/Student.java
@@ -7,6 +7,7 @@ import org.hibernate.annotations.OnDeleteAction;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.List;
 
 @Entity(name = "Student")
 @Table(name = "\"Students\"", schema = "public")
@@ -25,4 +26,37 @@ public class Student extends Person implements Serializable {
     @Getter
     @Setter
     private Integer state = 1;
+
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinTable(
+            name = "\"StudentCourse\"",
+            joinColumns = @JoinColumn(
+                    name = "student_id",
+                    referencedColumnName = "id",
+                    foreignKey = @ForeignKey(
+                            name = "fk_student_course",
+                            foreignKeyDefinition = "FOREIGN KEY (student_id)\n" +
+                                    "        REFERENCES public.\"Students\" (id) MATCH SIMPLE\n" +
+                                    "        ON UPDATE CASCADE\n" +
+                                    "        ON DELETE CASCADE",
+                            value = ConstraintMode.CONSTRAINT
+                    )
+            ),
+            inverseJoinColumns = @JoinColumn(
+                    name = "course_key",
+                    referencedColumnName = "key",
+                    foreignKey = @ForeignKey(
+                            name = "fk_course_student",
+                            foreignKeyDefinition = "FOREIGN KEY (course_key)\n" +
+                                    "        REFERENCES public.\"Courses\" (key) MATCH SIMPLE\n" +
+                                    "        ON UPDATE CASCADE\n" +
+                                    "        ON DELETE CASCADE",
+                            value = ConstraintMode.CONSTRAINT
+                    )
+            ),
+            uniqueConstraints = @UniqueConstraint(name = "composite_keys", columnNames = {"student_id", "course_key"})
+    )
+    @Getter
+    @Setter
+    private List<Course> enrolled_courses;
 }
